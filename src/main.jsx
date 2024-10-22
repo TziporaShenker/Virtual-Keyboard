c37c1d6a8f0f1c6a6fd67a9cb224475cbb0a560c
TziporaShenker <tzp81134@gmail.com>
2024-10-23 00:44:42 +0300

    Initial public commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import TextEditorApp from './TextEditorApp.jsx'
-import CarRentalForm from './CarRentalForm.jsx'
+import TextEditorApp from './TextEditorApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CarRentalForm/>
+    <TextEditorApp/>
   </React.StrictMode>,
 )
